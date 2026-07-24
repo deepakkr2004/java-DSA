11976e851fdb169555898d4d62b9eea0fc12ff38
deepakkr2004 <deepakkr315912@gmail.com>
2026-07-24 14:18:18 +0530

    Time: 2 ms (87.06%) | Memory: 49.2 MB (53.94%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -1,28 +1,70 @@
+//brutforce
+// class Solution {
+//     public void reorderList(ListNode head) {
+//         if(head.next==null || head.next.next==null) return;
+
+//         ArrayList<ListNode> list = new ArrayList<>();
+
+//         ListNode curr = head;
+//         while(curr != null){
+//             list.add(curr);
+//             curr = curr.next;
+//         }
+
+//         int left = 0;
+//         int right = list.size()-1;
+
+//         while(left < right){
+//             list.get(left).next = list.get(right);
+//             left++;
+
+//             if(left == right){
+//                 break;
+//             }
+//             list.get(right).next = list.get(left);
+//             right--;
+//         }
+//         list.get(left).next = null;
+//     }
+// }
+
+//optimize way
 class Solution {
     public void reorderList(ListNode head) {
         if(head.next==null || head.next.next==null) return;
 
-        ArrayList<ListNode> list = new ArrayList<>();
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
 
-        ListNode curr = head;
+        ListNode sec = slow.next;
+        slow.next = null;
+
+        ListNode prev = null;
+        ListNode curr = sec;
+        
         while(curr != null){
-            list.add(curr);
-            curr = curr.next;
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
 
-        int left = 0;
-        int right = list.size()-1;
+        ListNode first = head;
+        sec = prev;
 
-        while(left < right){
-            list.get(left).next = list.get(right);
-            left++;
+        while(sec != null){
+            ListNode next1 = first.next;
+            ListNode next2 = sec.next;
 
-            if(left == right){
-                break;
-            }
-            list.get(right).next = list.get(left);
-            right--;
+            first.next = sec;
+            sec.next = next1;
+
+            first = next1;
+            sec = next2;
         }
-        list.get(left).next = null;
     }
 }
