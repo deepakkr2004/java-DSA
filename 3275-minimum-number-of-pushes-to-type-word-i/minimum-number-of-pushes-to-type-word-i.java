0e6189b9a51e397498af61066741023e2a8537ef
deepakkr2004 <deepakkr315912@gmail.com>
2026-07-30 16:39:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (28.23%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -1,18 +1,15 @@
 class Solution {
     public int minimumPushes(String word) {
-
         int n = word.length();
+
         int ans = 0;
         int push = 1;
-
-        while (n >= 8) {
+        while(n >= 8){
             ans += 8 * push;
             n -= 8;
             push++;
         }
-
         ans += n * push;
-
         return ans;
     }
 }
