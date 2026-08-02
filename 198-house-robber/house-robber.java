9953cc6848bc08d8cc3aba9cc31ec1c4efd8c827
deepakkr2004 <deepakkr315912@gmail.com>
2026-08-02 20:50:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (88.56%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,19 +1,39 @@
+//Dp of tabulation tachnique
 class Solution {
-    public int amount(int[] nums,int i,int dp[]) {
-        if(i>=nums.length) return 0;
-
-        if (dp[i] != -1) return dp[i];
-
-        int take = nums[i] + amount(nums,i+2,dp);
-        int skip = amount(nums,i+1,dp);
-
-        return dp[i] = Math.max(take,skip);
-    }
-    public int rob(int[] nums) {
-        
-        int dp[] = new int[nums.length];
-        Arrays.fill(dp, -1);
-
-        return amount(nums,0,dp);
+    public int rob(int[] arr) {
+        int n = arr.length;
+        if(n == 1) return arr[0];
+        int[] dp = new int[n];
+        dp[0] = arr [0];
+        dp[1] = Math.max(arr[0] , arr[1]);
+        for(int i=2; i<n; i++){
+            dp[i] = Math.max(arr[i] + dp[i-2], dp[i-1]);
+        }
+        return dp[n-1];
     }
 }
+
+
+//DP of memorization technique
+// class Solution {
+//     public int amount(int[] nums,int i,int dp[]) {
+//         if(i>=nums.length) return 0;
+
+//         if (dp[i] != -1) return dp[i];
+
+//         int take = nums[i] + amount(nums,i+2,dp);
+//         int skip = amount(nums,i+1,dp);
+
+//         return dp[i] = Math.max(take,skip);
+//     }
+//     public int rob(int[] nums) {
+
+//         //i will varies from 0 to n-1
+//         //dp[i] will store the value of amount(i)
+
+//         int dp[] = new int[nums.length];
+//         Arrays.fill(dp, -1);
+
+//         return amount(nums,0,dp);
+//     }
+// }
