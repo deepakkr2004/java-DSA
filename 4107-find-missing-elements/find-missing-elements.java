13c17ec0ee4e24d1e0f52425757e8613e24119d3
deepakkr2004 <deepakkr315912@gmail.com>
2026-08-04 14:03:40 +0530

    Time: 6 ms (39.24%) | Memory: 46.8 MB (53.61%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.java
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -1,22 +1,46 @@
+// class Solution {
+//     public List<Integer> findMissingElements(int[] nums) {
+
+//         int min = nums[0];
+//         int max = nums[0];
+
+//         HashSet<Integer> set = new HashSet<>();
+
+//         for (int num : nums) {
+//             min = Math.min(min, num);
+//             max = Math.max(max, num);
+//             set.add(num);
+//         }
+
+//         List<Integer> ans = new ArrayList<>();
+
+//         for (int i = min + 1; i < max; i++) {
+//             if (!set.contains(i)) {
+//                 ans.add(i);
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
+
+import java.util.*;
+
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
 
-        int min = nums[0];
-        int max = nums[0];
-
-        HashSet<Integer> set = new HashSet<>();
-
-        for (int num : nums) {
-            min = Math.min(min, num);
-            max = Math.max(max, num);
-            set.add(num);
-        }
+        Arrays.sort(nums);
 
         List<Integer> ans = new ArrayList<>();
 
-        for (int i = min + 1; i < max; i++) {
-            if (!set.contains(i)) {
-                ans.add(i);
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            int curr = nums[i];
+            int next = nums[i + 1];
+
+            while (curr + 1 < next) {
+                curr++;
+                ans.add(curr);
             }
         }
 
