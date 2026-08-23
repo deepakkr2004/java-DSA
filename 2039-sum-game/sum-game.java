a408cf9c8d7ac13ebcbd164e5fa82f24dae0eed6
deepakkr2004 <deepakkr315912@gmail.com>
2026-08-23 13:59:36 +0530

    Time: 9 ms (29.93%) | Memory: 47 MB (64.23%) - LeetSync

--- 2039-sum-game/sum-game.java
+++ 2039-sum-game/sum-game.java
@@ -1,10 +1,13 @@
 class Solution {
     public boolean sumGame(String num) {
         int n = num.length();
+
         int leftSum = 0;
         int rightSum = 0;
+
         int leftSymbol = 0;
         int rightSymbol = 0;
+
         for(int i=0; i<n/2; i++){
             if(num.charAt(i) == '?'){
                 leftSymbol++;
@@ -16,13 +19,14 @@ class Solution {
         for(int i=n/2; i<n; i++){
             if(num.charAt(i) == '?'){
                 rightSymbol++;
-            }else
-            rightSum += num.charAt(i) - '0';
+            }else{
+                rightSum += num.charAt(i) - '0';
+            }
         }
-        int reqSum = rightSum - leftSum; 
-        if((rightSymbol + leftSymbol) % 2 == 1){
+       
+        if((rightSymbol + leftSymbol) % 2 == 1 || (rightSum - leftSum) != (leftSymbol - rightSymbol) * 9/2){
             return true;
         }
-        return reqSum != (leftSymbol - rightSymbol) * 9/2;
+        return false;
     }
 }
