cc1c1a564ffde6812a3651fe30a749de14201469
deepakkr2004 <deepakkr315912@gmail.com>
2026-08-24 13:52:55 +0530

    Time: 3 ms (86.87%) | Memory: 43.1 MB (85.66%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -1,13 +1,42 @@
+// class Solution {
+//     public int heightChecker(int[] heights) {
+//         int[] need = heights.clone();
+//         Arrays.sort(need);
+//         int count = 0;
+//         for(int i=0; i<heights.length; i++){
+//             if(need[i] != heights[i]){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+//through bubble sort
 class Solution {
     public int heightChecker(int[] heights) {
-        int[] need = heights.clone();
-        Arrays.sort(need);
-        int count = 0;
-        for(int i=0; i<heights.length; i++){
-            if(need[i] != heights[i]){
-                count++;
+
+        int[] expected = heights.clone();
+
+        for (int i = 0; i < expected.length - 1; i++) {
+            for (int j = 0; j < expected.length - 1 - i; j++) {
+
+                if (expected[j] > expected[j + 1]) {
+                    int temp = expected[j];
+                    expected[j] = expected[j + 1];
+                    expected[j + 1] = temp;
+                }
             }
         }
-        return count;
+
+        int ans = 0;
+
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != expected[i]) {
+                ans++;
+            }
+        }
+
+        return ans;
     }
 }
