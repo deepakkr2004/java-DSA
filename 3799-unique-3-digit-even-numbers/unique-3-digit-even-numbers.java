666a3ec0d79633ac363a1468c4bc9793e2fcfce5
deepakkr2004 <deepakkr315912@gmail.com>
2026-09-11 21:27:54 +0530

    Time: 10 ms (24.00%) | Memory: 47.2 MB (7.47%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        boolean[] ok = new boolean[1000];
+        int count =0;
+        for(int num=100; num<=998; num++){
+            if(num % 2 != 0){
+                continue;
+            }
+
+            int a = num/100;
+            int b = (num/10) % 10;
+            int c = num % 10;
+
+            int[] freq = new int[10];
+            for(int d : digits){
+                freq[d]++;
+            }
+            if(freq[a]>0){
+                freq[a]--;
+            }else{
+                continue;
+            }
+            if(freq[b]>0){
+                freq[b]--;
+            }else{
+                continue;
+            }
+            if(freq[c]>0){
+                ok[num] = true;
+                count++;
+            }
+        }
+        return count;
+    }
+}
