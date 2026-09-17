e25edfcb364199c59ff86db9d85b36813f35bf40
deepakkr2004 <deepakkr315912@gmail.com>
2026-09-17 19:14:34 +0530

    Time: 14 ms (5.63%) | Memory: 52.4 MB (7.73%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,53 +1,25 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        for(int row=0; row<9; row++){
-            HashSet<Character> set = new HashSet<>();
-            for(int col=0; col<9; col++){
-                if(board[row][col] == '.'){
-                    continue;
-                }
-                if(set.contains(board[row][col])){
-                    return false;
-                }
-                set.add(board[row][col]);
-            }
-        }
-        for(int col=0; col<9; col++){
-            HashSet<Character> set = new HashSet<>();
-            for(int row=0; row<9; row++){
-                if(board[row][col] == '.'){
-                    continue;
-                }
-                if(set.contains(board[row][col])){
-                    return false;
-                }
-                set.add(board[row][col]);
-            }
-        }
-        for(int sr=0; sr<9; sr+=3){
-            int er = sr+2;
-            for(int sc=0; sc<9; sc+=3){
-                int ec = sc+2;
-                if(!isValid(board, sr, er, sc, ec)){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    private boolean isValid(char[][] board, int sr, int er, int sc, int ec){
-        HashSet<Character> set = new HashSet<>();
-        for(int i=sr; i<=er; i++){
-            for(int j=sc; j<=ec; j++){
+        HashSet<String> set = new HashSet<>();
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
                 if(board[i][j] == '.'){
                     continue;
                 }
-                if(set.contains(board[i][j])){
+                String row = board[i][j] + "_ROW_" + i;
+                String col = board[i][j] + "_COL_" + j;
+                String box = board[i][j] + "_BOX_" + (i/3) + "_" + (j/3);
+
+                if(set.contains(row) ||
+                    set.contains(col) ||
+                    set.contains(box)){
                     return false;
                 }
-                set.add(board[i][j]);
+                set.add(row);
+                set.add(col);
+                set.add(box); 
             }
-        }
+        } 
         return true;
     }
 }
